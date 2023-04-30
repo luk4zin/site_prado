b72598f0c9018bd94833629fa6eb7adc373ee79c
Gabriel Luca <85700234+luk4zin@users.noreply.github.com>
2023-04-30 14:53:10 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <title> Site institucional Prado</title>
+
+
     <!-- MDB icon -->
     <link rel="icon" href="img/favicon.ico" type="image/x-icon" />
     <!-- Font Awesome -->
@@ -26,7 +28,53 @@
   </head>
   <body>
     <!-- Start your project here-->
-    
+    <div class="container-fluid">
+        <div class="back" >
+            <img src="./img/Prancheta-KoKero-InicialArtboard-2.png" class="img-fluid"/>
+        </div>
+    </div>
+    <div class="card" style="width: 400px;" >
+        <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+            <img src="./img/Camisa-NFL-32Artboard-2.png" class="img-fluid" />
+            <a href="#!">
+                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+            </a>
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Camisa da NFL Bengals 2008</h5>
+            <p class="card-text">Camisa da NFL de Rudi Johnson, ótimo estado, com apenas aguns detalhes na manga direita.</p>
+            <p class="card-text">R$ 180,00</p>
+            <a href="#!" class="btn btn-primary">Comprar</a>
+        </div>
+    </div>
+    <div class="card" style="width: 400px;" >
+        <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+            <img src="./img/Camisa-NFL-32Artboard-9.png" class="img-fluid" />
+            <a href="#!">
+                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+            </a>
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Regata duplaface Adidas</h5>
+            <p class="card-text">Perfeita para a prática de qualquer esporte, podendo ser usada de ambos os lados, perfeito estado .</p>
+            <p class="card-text">R$ 80,00</p>
+            <a href="#!" class="btn btn-primary">Comprar</a>
+        </div>
+    </div>
+    <div class="card" style="width: 400px;" >
+        <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+            <img src="./img/11Artboard-3.png" class="img-fluid" />
+            <a href="#!">
+                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+            </a>
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Blusa da Seleção Brasileira de Vôlei</h5>
+            <p class="card-text">Dos anos 80/90, da seleção de ouro, perfeito estado, sendo da marca "Rainha".</p>
+            <p class="card-text">R$ 200,00</p>
+            <a href="#!" class="btn btn-primary">Comprar</a>
+        </div>
+    </div>
     <!-- End your project here-->
 
     <!-- MDB -->
